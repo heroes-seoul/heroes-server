ecad3f1df2a1590d1d1a88bbe33e8c5e4ec1543d
iiqcov <iiqcov@gmail.com>
2024-08-06 10:34:39 +0900

    [FIX] health 컨트롤러 추가

---
+++ src/main/java/heroes/HealthController.java
@@ -0,0 +1,14 @@
+package heroes;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
