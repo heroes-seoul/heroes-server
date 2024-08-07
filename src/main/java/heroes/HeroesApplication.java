d8356a5e5bc98932bc614d26903ee7d0b67770b6
iiqcov <iiqcov@gmail.com>
2024-08-07 20:53:07 +0900

    [FEAT] BaseTimeEntity 추가

--- src/main/java/heroes/HeroesApplication.java
+++ src/main/java/heroes/HeroesApplication.java
@@ -2,8 +2,10 @@ package heroes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HeroesApplication {
 
     public static void main(String[] args) {
