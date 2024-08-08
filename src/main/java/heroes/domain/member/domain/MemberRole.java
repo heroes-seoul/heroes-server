6aafb88a93c3cebc55f999e1b284e1d8e35c0955
iiqcov <iiqcov@gmail.com>
2024-08-09 00:34:16 +0900

    [FIX] company 역할 설정 추가

--- src/main/java/heroes/domain/member/domain/MemberRole.java
+++ src/main/java/heroes/domain/member/domain/MemberRole.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberRole {
-    USER("ROLE_USER");
+    USER("ROLE_USER"),
+    COMPANY("ROLE_COMPANY");
 
     private final String value;
 }
