1a2bfbf1eb1c8322a9184ff3e6d7d3ed5a1802fe
iiqcov <iiqcov@gmail.com>
2024-08-09 10:57:33 +0900

    [FEAT] kakao 및 jwt 환경변수 설정

---
+++ src/main/java/heroes/infra/config/jwt/JwtProperties.java
@@ -0,0 +1,24 @@
+package heroes.infra.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class JwtProperties {
+    private String accessTokenSecret;
+    private String refreshTokenSecret;
+    private Long accessTokenExpirationTime;
+    private Long refreshTokenExpirationTime;
+    private String issuer;
+
+    public Long accessTokenExpirationMilliTime() {
+        return accessTokenExpirationTime * 1000;
+    }
+
+    public Long refreshTokenExpirationMilliTime() {
+        return refreshTokenExpirationTime * 1000;
+    }
+}
---
+++ src/main/java/heroes/infra/config/oauth/KakaoProperties.java
@@ -0,0 +1,15 @@
+package heroes.infra.config.oauth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.kakao.client")
+@Getter
+@Setter
+public class KakaoProperties {
+    private String id;
+    private String secret;
+    private String redirectUri;
+    private String grantType;
+}
