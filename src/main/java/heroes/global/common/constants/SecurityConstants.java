16a7dffeed7ebce8d1dc3c5b447a092b3532ff17
iiqcov <iiqcov@gmail.com>
2024-08-11 13:16:32 +0900

    [FEAT] 회원탈퇴를 위한 상수 정의

--- src/main/java/heroes/global/common/constants/SecurityConstants.java
+++ src/main/java/heroes/global/common/constants/SecurityConstants.java
@@ -7,6 +7,13 @@ public final class SecurityConstants {
     public static final String KAKAO_LOGIN_URL = "https://kauth.kakao.com";
     public static final String KAKAO_LOGIN_ENDPOINT = "/oauth/token";
 
+    public static final String KAKAO_WITHDRAWAL_URL = "https://kapi.kakao.com";
+    public static final String KAKAO_WITHDRAWAL_ENDPOINT = "/v1/user/unlink";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String KAKAO_WITHDRAWAL_HEADER_PREFIX = "KakaoAK ";
+    public static final String KAKAO_WITHDRAWAL_TYPE = "user_id";
+    public static final String KAKAO_WITHDRAWAL_URL_ENCODING = "application/x-www-form-urlencoded";
+
     public static final String KAKAO_ISSUER = "https://kauth.kakao.com";
     public static final String KAKAO_JWK_SET_URL = "https://kauth.kakao.com/.well-known/jwks.json";
 }
