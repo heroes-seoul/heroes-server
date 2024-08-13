a504a6667ce86312c56bfc89be774f506a4c464e
OKSUchun <oksuchun119@gmail.com>
2024-08-13 21:08:03 +0900

    [FEAT] CompanyUtil 생성 - securityUtil > memberUtil > CompanyUtil - securityContextHolder의 MemberId 로부터 등록된 Company 확인할 수 있음

---
+++ src/main/java/heroes/global/util/CompanyUtil.java
@@ -0,0 +1,22 @@
+package heroes.global.util;
+
+import heroes.domain.company.dao.CompanyRepository;
+import heroes.domain.company.domain.Company;
+import heroes.domain.member.dao.MemberRepository;
+import heroes.global.error.exception.CustomException;
+import heroes.global.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CompanyUtil {
+    private final MemberUtil memberUtil;
+    private final CompanyRepository companyRepository;
+
+    public Company getCurrentCompany() {
+        return companyRepository
+                .findById(memberUtil.getCurrentMember().getId())
+                .orElseThrow(() -> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+    }
+}
