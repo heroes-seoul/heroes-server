cfdea33d60bdfc82447ddd33c748da672e9d58ab
iiqcov <iiqcov@gmail.com>
2024-08-13 12:17:59 +0900

    [CHORE] s3 환경변수 설정

--- src/main/java/heroes/infra/config/PropertiesConfig.java
+++ src/main/java/heroes/infra/config/PropertiesConfig.java
@@ -2,9 +2,14 @@ package heroes.infra.config;
 
 import heroes.infra.config.jwt.JwtProperties;
 import heroes.infra.config.oauth.KakaoProperties;
+import heroes.infra.config.storage.StorageProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({KakaoProperties.class, JwtProperties.class})
+@EnableConfigurationProperties({
+    KakaoProperties.class,
+    JwtProperties.class,
+    StorageProperties.class
+})
 @Configuration
 public class PropertiesConfig {}
