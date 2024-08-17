b0fe933d247866ca39ab03c0fea62a00e394d27f
OKSUchun <oksuchun119@gmail.com>
2024-08-17 19:18:49 +0900

    [CHORE] DayOfWeek MessageConstants 추가

--- src/main/java/heroes/global/common/constants/MessageConstants.java
+++ src/main/java/heroes/global/common/constants/MessageConstants.java
@@ -2,4 +2,5 @@ package heroes.global.common.constants;
 
 public final class MessageConstants {
     public static final String INVALID_IMAGE_TYPE_MESSAGE = "이미지 유형이 잘못되었습니다.";
+    public static final String INVALID_DAYOFWEEK_MESSAGE = "요일 입력이 잘못되었습니다.";
 }
